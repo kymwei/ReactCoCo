abc228e8442337350bb50d7fe0edb1e45d6a3b94
kymwei <kymwei1002@gmail.com>
2016-03-08 23:10:03 -0800

    yea

--- example/react-reading-time.jsx
+++ example/react-reading-time.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+export default class ReadingTime extends React.Component {
+    static propTypes = {
+        wordsPerMinute: React.PropTypes.number
+    };
+
+    static defaultProps = {
+        wordsPerMinute: 270
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            readTime: 0
+        };
+    }
+
+    render() {
+        return (
+            <div>Hello ReadingTime!</div>
+        );
+    }
+}
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,32 +1,42 @@
-var webpack = require('webpack');
+//var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        'react-reading-time': [
-            'webpack-dev-server/client?http://localhost:8881/',
-            'webpack/hot/only-dev-server',
-            './example/react-reading-time.jsx'
-        ]
-    },
-    output: {
-        path: __dirname,
-        filename: "[name].js",
-        publicPath: 'http://localhost:8881/',
-        chunkFilename: '[id].chunk.js',
-        sourceMapFilename: '[name].map'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.es6'],
-        modulesDirectories: ['node_modules']
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx$|\.es6$|\.js$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/ },
-            { test: /\.scss$|\.css$/, loader: 'style-loader!style!css!sass' }
-        ]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin()
-    ],
-    devtool: "eval-source-map"
+  entry: {
+    'react-reading-time': [
+      'webpack-dev-server/client?http://localhost:8881/',
+      'webpack/hot/only-dev-server',
+      './example/react-reading-time.jsx'
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].js",
+    publicPath: 'http://localhost:8881/',
+    chunkFilename: '[id].chunk.js',
+    sourceMapFilename: '[name].map'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.es6'],
+    modulesDirectories: ['node_modules']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx$/, /\.es6$/,/\.js$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+        //,
+        //query: {
+        //  presets: ['react', 'es2015','stage-0'],
+        //  plugins:['transform-runtime']
+        //}
+      },
+      { test: /\.scss$|\.css$/, loader: 'style-loader!style!css!sass' }
+    ]
+  },
+  plugins: [
+    //new webpack.NoErrorsPlugin()
+  ],
+  devtool: "eval-source-map"
 };
+
